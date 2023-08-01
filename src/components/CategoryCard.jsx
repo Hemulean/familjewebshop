290569c252e998efb88321adfff9ed733499972e
Emil Nilsson <86913330+Hemulean@users.noreply.github.com>
2023-08-01 16:25:27 +0200

    fixed some responsive grid layout

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function CategoryCards() {
   return (
-    <div className="mx-4 grid lg:grid-cols-3 gap-3 lg:text-2xl md:text-xl sm:text-md ">
-      <div class="relative overflow-hidden  cursor-pointer  col-span-2 row-span-2 ">
-        <p className="card-text">Konstverk</p>
+    <div className="mx-4 grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 lg:grid-rows-2 gap-2 lg:text-2xl md:text-xl sm:text-sm mx-1 ">
+      <div className="relative overflow-hidden  cursor-pointer col-span-2 row-span-2 ">
+        <p className="card-text zoom">Konstverk</p>
 
         <img
           class="object-cover card-img"
@@ -12,32 +12,33 @@ function CategoryCards() {
           alt="Flower and sky"
         />
       </div>
-      <div className="relative  cursor-pointer  ">
-        <p className="card-text">Inredning</p>
+      <div className="relative overflow-hidden category-cards   cursor-pointer  ">
+        <p className="card-text zoom">Inredning</p>
         <img
-          class="object-cover card-img"
+          className="object-cover card-img"
           src="/img/furniture.jpg"
           alt="furniture"
         />
       </div>
-      <div className="relative overflow-hidden  cursor-pointer  ">
-        <p className="card-text">Smycken</p>
+
+      <div className="relative overflow-hidden category-cards     cursor-pointer  ">
+        <p className="card-text zoom">Smycken</p>
         <img
           class="object-cover card-img"
           src="/img/jewelry.jpg"
           alt="furniture"
         />
       </div>
-      <div className="relative overflow-hidden  cursor-pointer  ">
-        <p className="card-text">Vintage</p>
+      <div className="relative overflow-hidden   cursor-pointer  ">
+        <p className="card-text zoom">Vintage</p>
         <img
           class="object-cover card-img"
           src="/img/vintage.jpg"
           alt="furniture"
         />
       </div>
-      <div className="relative overflow-hidden  cursor-pointer  ">
-        <p className="card-text">Tillbehör</p>
+      <div className="relative overflow-hidden lg:col-span-2  cursor-pointer  ">
+        <p className="card-text zoom">Tillbehör</p>
         <img
           class="object-cover card-img"
           src="/img/tillbehör.jpg"
